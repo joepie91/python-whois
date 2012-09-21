e7fdf1c52ce6ab6f4ace2eb014e3d953c85e7c81
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-09-21 17:08:55 +0200

    Include setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pythonwhois',
+      version='1.0.0',
+      description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains.',
+      author='Sven Slootweg',
+      author_email='pythonwhois@cryto.net',
+      url='http://cryto.net/pythonwhois',
+      packages=['pythonwhois'],
+      provides=['pythonwhois']
+     )
