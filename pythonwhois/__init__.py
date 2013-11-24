910e74ab2b1d018f4d0ec5a0bf69bb96f23924ca
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-11-24 17:06:51 +0100

    Fix for missing 'normalized' keyword argument in get_whois

--- pythonwhois/__init__.py
+++ pythonwhois/__init__.py
@@ -1,8 +1,8 @@
 from . import net, parse
 
-def get_whois(domain):
+def get_whois(domain, normalized=[]):
 	raw_data = net.get_whois_raw(domain)
-	return parse.parse_raw_whois(raw_data)
+	return parse.parse_raw_whois(raw_data, normalized=normalized)
 
 def whois(*args, **kwargs):
 	raise Exception("The whois() method has been replaced by a different method (with a different API), since pythonwhois 2.0. Either install the older pythonwhois 1.2.3, or change your code to use the new API.")
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pythonwhois',
-      version='2.0.0',
+      version='2.0.1',
       description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains. No dependencies.',
       author='Sven Slootweg',
       author_email='pythonwhois@cryto.net',
