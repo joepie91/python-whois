9b16e995820d846f31bec641dedfe8fbf73d2d90
Sven Slootweg <jamsoftgamedev@gmail.com>
2014-04-02 09:24:17 +0200

    Version bump to 2.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pythonwhois',
-      version='2.0.5',
+      version='2.1.0',
       description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains. No dependencies.',
       author='Sven Slootweg',
       author_email='pythonwhois@cryto.net',
