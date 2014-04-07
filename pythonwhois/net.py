a7367593d3771917ad9b6b677939550ad188bc89
Sven Slootweg <jamsoftgamedev@gmail.com>
2014-04-07 21:26:47 +0200

    Whitespace fixes

--- pythonwhois/net.py
+++ pythonwhois/net.py
@@ -4,8 +4,8 @@ from . import shared
 
 def get_whois_raw(domain, server="", previous=[], rfc3490=True):
 
-        if rfc3490:
-            domain = encode( domain if type(domain) is unicode else decode(domain, "utf8"), "idna" )
+	if rfc3490:
+		domain = encode( domain if type(domain) is unicode else decode(domain, "utf8"), "idna" )
 
 	if len(previous) == 0:
 		# Root query
@@ -14,8 +14,8 @@ def get_whois_raw(domain, server="", previous=[], rfc3490=True):
 		target_server = server
 	if domain.endswith(".jp") and target_server == "whois.jprs.jp":
 		request_domain = "%s/e" % domain # Suppress Japanese output
-        elif domain.endswith(".de") and ( target_server == "whois.denic.de" or target_server == "de.whois-servers.net" ):
-                request_domain = "-T dn,ace %s" % domain # regional specific stuff
+	elif domain.endswith(".de") and ( target_server == "whois.denic.de" or target_server == "de.whois-servers.net" ):
+		request_domain = "-T dn,ace %s" % domain # regional specific stuff
 	elif target_server == "whois.verisign-grs.com":
 		request_domain = "=%s" % domain # Avoid partial matches
 	else:
