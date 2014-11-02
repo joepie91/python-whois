88872fe78f90f9a84b06ebb3544a2a964536746f
rynmccrmck <ryanjmccormack@gmail.com>
2014-11-02 17:38:12 -0500

    Update parse.py
    updated for .ca

--- pythonwhois/parse.py
+++ pythonwhois/parse.py
@@ -312,6 +312,7 @@ admin_contact_regexes = [
 	"   Administrative Contact:\n      (?P<name>.+)  (?P<email>.+)\n      (?P<phone>.*)\n      (?P<fax>.*)\n", # .com.tw (Western registrars)
 	"Administrative Contact Information:\n\n(?:Given name: (?P<firstname>.+)\n)?(?:Family name: (?P<lastname>.+)\n)?(?:Company name: (?P<organization>.+)\n)?Address: (?P<street>.+)\nCountry: (?P<country>.+)\nPhone: (?P<phone>.*)\nFax: (?P<fax>.*)\nEmail: (?P<email>.+)\n(?:Account Name: (?P<handle>.+)\n)?", # HKDNR (.hk)
 	"ADMIN ID:(?P<handle>.+)\nADMIN Name:(?P<name>.*)\n(?:ADMIN Organization:(?P<organization>.*)\n)?ADMIN Street1:(?P<street1>.+?)\n(?:ADMIN Street2:(?P<street2>.+?)\n(?:ADMIN Street3:(?P<street3>.+?)\n)?)?ADMIN City:(?P<city>.+)\nADMIN State:(?P<state>.*)\nADMIN Postal Code:(?P<postalcode>.+)\nADMIN Country:(?P<country>[A-Z]+)\nADMIN Phone:(?P<phone>.*?)\nADMIN Fax:(?P<fax>.*)\nADMIN Email:(?P<email>.+)\n", # Realtime Register
+	"\nAdministrative contact:\n\s*Name:\s*(?P<name>.+)\n\s*Postal address:.+\n\s*(?P<city>.*).*\s(?P<state>(?:AB|BC|MB|NB|NL|NS|ON|PE|SK|QC)).+\s(?P<country>.+).*\n\s*Phone:\s*(?P<phone>.+)\n.+\n\s*Email:\s*(?P<email>.+)", #.ca registrar
 ]
 
 billing_contact_regexes = [
@@ -553,7 +554,10 @@ def parse_raw_whois(raw_data, normalized=None, never_query_handles=True, handle_
 					data["nameservers"].append(match.strip())
 				except KeyError as e:
 					data["nameservers"] = [match.strip()]
-		
+		# .ca fix
+		match = re.search("Registrar:\n\s*Name:\s*(.+)\n", segment)
+                if match is not None:
+                        data["registrar"] = [match.group(1).strip()]
 
 	data["contacts"] = parse_registrants(raw_data, never_query_handles, handle_server)
 
