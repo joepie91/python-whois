97c2045be4c165d0c00be4fe7a91c21a7ca9d35c
Sander <s.ten.hoor@outlook.com>
2016-06-10 12:15:57 +0200

    ADD: The values for the default cool down and the cool down period can now be configured

--- pythonwhois/ratelimit/cool_down.py
+++ pythonwhois/ratelimit/cool_down.py
@@ -3,20 +3,31 @@ import thread
 import threading
 import time
 
-default_cool_down_length = 1.0
-cool_down_period = 0.5
-
 
 def decrement_thread(cool_down_object):
     """
     After sleeping for cool_down_time, decrement
     all cool downs with cool_down_time
     :param cool_down_object:
-    :return:
     """
     while True:
-        time.sleep(cool_down_period)
+        time.sleep(cool_down_object.cool_down_period)
         cool_down_object.decrement_cool_downs()
+
+
+def get_from_config(config, section, key, default=None):
+    """
+    Get a value from the config if it exists, otherwise return the default value
+    :param config: The configuration to get the value from
+    :param section: The section to get the value from
+    :param key: The key that may or may not exist
+    :param default: The default value to return, which is None by default
+    :return: The value if it exists, else default
+    """
+    if config.has_option(section, key):
+        return config.getfloat(section, key)
+    else:
+        return default
 
 
 class CoolDown:
@@ -30,6 +41,8 @@ class CoolDown:
         """
         self.lock = threading.Lock()
         self.servers_on_cool_down = {}
+        self.default_cool_down_length = 1.0
+        self.cool_down_period = 0.5
         thread.start_new_thread(decrement_thread, (self,))
 
     def can_use_server(self, whois_server):
@@ -50,7 +63,7 @@ class CoolDown:
         """
         with self.lock:
             if whois_server not in self.servers_on_cool_down:
-                self.servers_on_cool_down[whois_server] = CoolDownTracker(default_cool_down_length)
+                self.servers_on_cool_down[whois_server] = CoolDownTracker(self.default_cool_down_length)
             self.servers_on_cool_down[whois_server].use()
 
     def decrement_cool_downs(self):
@@ -59,7 +72,7 @@ class CoolDown:
         """
         with self.lock:
             for server, cool_down in self.servers_on_cool_down.iteritems():
-                self.servers_on_cool_down[server].decrement_cooldown(cool_down_period)
+                self.servers_on_cool_down[server].decrement_cooldown(self.cool_down_period)
 
     def set_cool_down_config(self, path_to_file):
         """
@@ -70,30 +83,32 @@ class CoolDown:
         """
         config = ConfigParser.ConfigParser()
         config.read(path_to_file)
+        config = self.get_and_remove_defaults_from_config(config)
         for domain in config.sections():
-            cool_down_length = self.get_from_config(config, domain, "cool_down_length", default_cool_down_length)
-            max_requests_minute = self.get_from_config(config, domain, "max_requests_minute")
-            max_requests_hour = self.get_from_config(config, domain, "max_requests_hour")
-            max_requests_day = self.get_from_config(config, domain, "max_requests_day")
+            cool_down_length = get_from_config(config, domain, "cool_down_length", self.default_cool_down_length)
+            max_requests_minute = get_from_config(config, domain, "max_requests_minute")
+            max_requests_hour = get_from_config(config, domain, "max_requests_hour")
+            max_requests_day = get_from_config(config, domain, "max_requests_day")
             with self.lock:
                 self.servers_on_cool_down[domain] = CoolDownTracker(cool_down_length,
                                                                     max_requests_minute,
                                                                     max_requests_hour,
                                                                     max_requests_day)
 
-    def get_from_config(self, config, section, key, default=None):
+    def get_and_remove_defaults_from_config(self, config):
         """
-        Get a value from the config if it exists, otherwise return the default value
-        :param config: The configuration to get the value from
-        :param section: The section to get the value from
-        :param key: The key that may or may not exist
-        :param default: The default value to return, which is None by default
-        :return: The value if it exists, else default
+        Gets the general settings from the config. Then removes them
+        and returns the modified config.
+        :param config: The config to obtain the default values from
+        :return: The modified config, without the 'general' section
         """
-        if config.has_option(section, key):
-            return config.getfloat(section, key)
-        else:
-            return default
+        if config.has_section("general"):
+            self.default_cool_down_length = get_from_config(config, "general", "default_cool_down_length",
+                                                            self.default_cool_down_length)
+            self.cool_down_period = get_from_config(config, "general", "cool_down_period", self.cool_down_period)
+            config.remove_section("general")
+
+        return config
 
 
 class CoolDownTracker:
